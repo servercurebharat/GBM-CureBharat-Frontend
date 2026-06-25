668f94df28f8b004ffdc93f025408b1111758bc5
Harshal152003 <harshladukar@gmail.com>
2026-06-25 15:12:32 +0530

    add backend link

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: process.env.NODE_ENV === 'development' 
+          ? 'http://localhost:5000/api/:path*' 
+          : 'https://mlm-backend-phi.vercel.app/api/:path*',
       },
     ];
   },
